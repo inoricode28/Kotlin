47c25cfa3f6876690e68dd7141f9fdf3eef9dd92
inoricode28 <inori237@gmail.com>
2024-06-21 17:01:56 -0500

    Complete_AppNAvigationScreen

--- Kotlin_COMPOSE_6TO/AppNavigationScreen/app/src/main/java/pe/edu/idat/appnavigationscreen/MainActivity.kt
+++ Kotlin_COMPOSE_6TO/AppNavigationScreen/app/src/main/java/pe/edu/idat/appnavigationscreen/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import pe.edu.idat.appnavigationscreen.ui.theme.AppNavigationScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppNavigationScreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pantallaA",
+                    builder = {
+                        composable("pantallaA"){
+                            PantallaA(navController)
+                        }
+                        composable("pantallaB"){
+                            PantallaB(navController)
+                        }
+                    })
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppNavigationScreenTheme {
-        Greeting("Android")
+
     }
 }
---
+++ Kotlin_COMPOSE_6TO/AppNavigationScreen/app/src/main/java/pe/edu/idat/appnavigationscreen/PantallaAScreen.kt
@@ -0,0 +1,30 @@
+package pe.edu.idat.appnavigationscreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun PantallaA(navController: NavController) {
+    Column(
+        Modifier
+            .padding(top = 25.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            navController.navigate("pantallaB")
+        }) {
+            Text(text = "Ir ala pantalla B")
+        }
+    }
+}
