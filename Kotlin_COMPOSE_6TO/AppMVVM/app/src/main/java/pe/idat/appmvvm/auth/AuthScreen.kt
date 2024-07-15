35727d31a4993ca51a9d7f61c98a124ae4682e2b
inoricode28 <inori237@gmail.com>
2024-07-14 20:43:28 -0500

    PassVisiable

--- Kotlin_COMPOSE_6TO/AppMVVM/app/src/main/java/pe/idat/appmvvm/auth/AuthScreen.kt
+++ Kotlin_COMPOSE_6TO/AppMVVM/app/src/main/java/pe/idat/appmvvm/auth/AuthScreen.kt
@@ -12,17 +12,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -71,19 +79,38 @@ fun txtusuario(usuario: String, onTextChanged: (String) -> Unit){
         modifier = Modifier.fillMaxWidth(),
         label = { Text(text = "Usuario") },
         maxLines = 1,
-        singleLine = true
+        singleLine = true ,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
     )
 }
 
 @Composable
 fun txtpassword(password: String, onTextChanged: (String) -> Unit){
+    var visible by rememberSaveable {
+        mutableStateOf(false)
+    }
     OutlinedTextField(value = password,
         onValueChange = { onTextChanged(it) },
         modifier = Modifier.fillMaxWidth(),
         label = { Text(text = "Password") },
         maxLines = 1,
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imagen = if(visible){
+                Icons.Filled.VisibilityOff
+            }else{
+                Icons.Filled.Visibility
+            }
+            IconButton(onClick = { visible = !visible }) {
+                Icon(imageVector = imagen, contentDescription = "password")
+                
+            }
+
+        },
+        visualTransformation = if (visible){
+            VisualTransformation.None
+        }else PasswordVisualTransformation()
     )
 }
 
